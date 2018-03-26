63c52d924ab44c6c6dc54686047c223dd39c1593
Zhao Yinji <zhao.yinji@parkman.io>
2018-03-26 14:08:10 +0300

    checkstyle fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param someone
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
